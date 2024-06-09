9cf1ee1e9ea7403118462ce211966cfe9139fa0b
Daksh Sawhney <darkstardaksh@gmail.com>
2024-06-09 15:01:22 +0530

    new

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-let box = document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
 
+let turnO;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+let checkWinner = () => {
+  for (let pattern of winPatterns) {
+    console.log(pattern[0], pattern[1], pattern[2]);
+  }
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("Box Was Clicked");
+    if (turnO === true) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
