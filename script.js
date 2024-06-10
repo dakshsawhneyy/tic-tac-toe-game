f3acb784b3c1b0c74e8758dfbcd7a17098111bac
Daksh Sawhney <darkstardaksh@gmail.com>
2024-06-10 13:50:26 +0530

    Final

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
+let newBtn = document.querySelector("#new");
+let topPara = document.querySelector("#top");
+let topDiv = document.querySelector(".top-text");
+let h1 = document.querySelector(".h1");
 
-let turnO;
+let winO;
+let clickCount = 0;
+
+let resetGame = () => {
+  enableBoxes();
+  topDiv.classList.add("hide");
+  resetCount();
+};
+
+newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+let click = () => {
+  clickCount++;
+  if (clickCount === 9) {
+    console.log("Game is draw");
+    topPara.innerText = "Game Is Draw, Try Again";
+    topDiv.classList.remove("hide");
+  }
+};
+
+let resetCount = () => {
+  clickCount = 0;
+};
 
 const winPatterns = [
   [0, 1, 2],
@@ -14,24 +42,51 @@ const winPatterns = [
   [6, 7, 8],
 ];
 
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (winO === true) {
+      box.innerText = "O";
+      winO = false;
+    } else {
+      box.innerText = "X";
+      winO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+
+    click();
+  });
+});
+
 let checkWinner = () => {
   for (let pattern of winPatterns) {
-    console.log(pattern[0], pattern[1], pattern[2]);
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner is", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
   }
 };
 
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    console.log("Box Was Clicked");
-    if (turnO === true) {
-      box.innerText = "O";
-      turnO = false;
-    } else {
-      box.innerText = "X";
-      turnO = true;
-    }
-    box.disabled = true;
+let showWinner = (winner) => {
+  topPara.innerText = `Congratulations, Winner is ${winner}`;
+  topDiv.classList.remove("hide");
+  disableBoxes();
+};
 
-    checkWinner();
-  });
-});
+let disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+let enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
